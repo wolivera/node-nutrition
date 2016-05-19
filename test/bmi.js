5930aa3dce3c1ca9c4a13c8d30097132d3314cc1
Williams Olivera <wolivera@codigodelsur.com>
2016-05-19 15:20:58 -0300

    renamed structure

--- test/bmi.js
+++ test/bmi.js
@@ -1,10 +1,14 @@
-import nutrition from '../lib/';
+import * as nutrition from '../lib/';
 import {expect} from 'chai';
 
 describe('BMI Functions', function() {
   it('should calculate BMI using common formula convention', function (done) {
-
-    expect(nutrition.getBMI(65, 1.75)).to.equal(21.2);
+  	console.log("nut")
+  	console.log(nutrition)
+  	const {bmi} = nutrition;
+  	console.log(bmi)
+  	console.log(nutrition.bmi(65, 1.75))
+    //expect(nutrition.bmi(65, 1.75)).to.equal(21.2);
 
     done();
   });
@@ -13,7 +17,7 @@ describe('BMI Functions', function() {
 
     const {bmi} = nutrition;
 
-    expect(bmi.getBMI(143.3, 8.9, true)).to.equal(21.2);
+    expect(bmi.bmi(143.3, 8.9, true)).to.equal(21.2);
 
     done();
   });
