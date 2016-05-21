0038e58462686f286074f9650e465c142136bf7f
Williams Olivera <wolivera@codigodelsur.com>
2016-05-20 21:27:12 -0300

    tests passing

--- test/bmi.js
+++ test/bmi.js
@@ -1,18 +1,14 @@
-import * as nutrition from '../lib/';
-import {expect} from 'chai';
+import * as nutrition from '../lib/'
+import {expect} from 'chai'
 
-describe('BMI Functions', function() {
-  it('should calculate BMI using common formula convention', function (done) { 
-    expect(nutrition.bmi(65, 1.75)).to.equal(21.2);
-    done();
-  });
+describe('BMI Functions', function () {
+  it('should calculate BMI using common formula convention', function (done) {
+    expect(nutrition.bmi(65, 1.75)).to.equal(21.22)
+    done()
+  })
 
   it('should calculate BMI using English formula convention', function (done) {
-
-    const {bmi} = nutrition;
-
-    expect(bmi.bmi(143.3, 8.9, true)).to.equal(21.2);
-
-    done();
-  });
-});
+    expect(nutrition.bmi(143, 69, true)).to.equal(21.12)
+    done()
+  })
+})
